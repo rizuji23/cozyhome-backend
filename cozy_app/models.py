713936e88096dc58ce30dbbfa74eb68801dfa649
root <root@cozyhome.weworks.ink>
2023-06-06 18:38:19 +0000

    server

--- cozy_app/models.py
+++ cozy_app/models.py
@@ -75,7 +75,7 @@ class Progress_Project(models.Model):
     desc = models.TextField(null=True)
     percentage = models.IntegerField(null=True)
     status = models.CharField(max_length=100)
-    foto = models.ImageField(upload_to='progress/', default='')
+    foto = models.FileField(upload_to='progress/', default='')
     id_user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -181,7 +181,7 @@ class Pekerjaan_Lain(models.Model):
 class User_Detail(models.Model):
     id = models.AutoField(primary_key=True)
     id_user_detail = models.CharField(max_length=100)
-    img = models.ImageField(upload_to='img_user/', default='profile.png')
+    img = models.FileField(upload_to='img_user/', default='profile.png')
     id_user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
     created_at = models.DateTimeField(auto_now_add=True, null=True)
     updated_at = models.DateTimeField(auto_now=True, null=True)
--- cozyhome/settings.py
+++ cozyhome/settings.py
@@ -154,6 +154,7 @@ AUTH_USER_MODEL = "cozy_app.User"
 STATIC_URL = "static/"
 MEDIA_URL = "media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
